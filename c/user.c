c02f61f7e356482f4a05095d2d7a427a00037690
Johnny Shen <johndoe17326@gmail.com>
2017-11-27 18:46:39 -0800

    test process 80% done

--- c/user.c
+++ c/user.c
@@ -18,7 +18,10 @@ void consumer( void ) {
 
 void test( void ) {
 
-	int sysopenTest,
+	int syssighandlerTest,
+		syskillTest,
+		syswaitTest,
+		sysopenTest,
 		syswriteTest,
 		sysioctlTest,
 		sysreadTest,
@@ -29,8 +32,13 @@ void test( void ) {
 //	2. syssighandler() test
 
 //	3. syskill() test
+	syskillTest = syskill( 0, 2525 );
+	kprintf( "syskill test result returns %d, expecting -561\n", syskillTest );
 
 //	4. syswait() test
+	syswaitTest = syswait( 0 );
+	kprintf( "trying to terminate idle process\n" );
+	kprintf( "syswait test result returns %d, expecting -1\n", syswaitTest );
 
 //	5. sysopen() test with invalid arguments
 	sysopenTest = sysopen( 5 );
@@ -44,16 +52,19 @@ void test( void ) {
 	kprintf( "syswrite test result returns %d, expecting -1\n", syswriteTest );
 
 //	7. sysioctl() test with invalid commands
-	sysopenTest = sysopen( 1 );		// 9. tests for uncovered cases (1/2)
-	sysioctlTest = sysioctl( 1, 66 );
+	sysopenTest = sysopen( 0 );
+	sysioctlTest = sysioctl( 0, 666 );
 	kprintf( "sysopen test result returns %d, expecting 1\n", sysopenTest );
 	kprintf( "sysioctl test result returns %d, expecting -1\n", sysioctlTest );
 
+	sysioctlTest = sysioctl( 0, 56 );	// 9. test for uncovered cases (1/2)
+	kprintf( "sysioctl test result returns %d, expecting 0\n", sysioctlTest );
+
 //	8. sysread() when there are more characters buffered in kernal than read request
-	sysreadTest = sysread( 1, buffer, sizeof(int) - 1 );
+	sysreadTest = sysread( 0, buffer, sizeof(int) - 1 );
 	kprintf( "sysread test result returns %d, expecting ??\n", sysreadTest );
 
-	syscloseTest = sysclose( 1 );	// 9. tests for uncovered cases (2/2)
+	syscloseTest = sysclose( 0 );	// 9. tests for uncovered cases (2/2)
 	kprintf( "sysclose test result returns %d, expecting 0\n", syscloseTest );
 
 	sysstop();
