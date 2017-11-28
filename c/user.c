e8c25b3aac4eae94512c16c20bf5a48b44fbc37b
Johnny Shen <johndoe17326@gmail.com>
2017-11-27 21:32:29 -0800

    testing results

--- c/user.c
+++ c/user.c
@@ -65,10 +65,10 @@ void     root( void ) {
 /****************************/
 
 	char input[256];
-	int *inputData = &input[0];
+	char *inputData = &input[0];
 
 	char pass[256];
-	int *userPass = &pass[0];
+	char *userPass = &pass[0];
 
 	char user[6] = { 'c', 's', '4', '1', '5', '\0' };
 
@@ -83,7 +83,6 @@ void     root( void ) {
 
 	char ps[2] = { 'p', 's' };
 	char ex[2] = { 'e', 'x' };
-	char EOF = ;
 	char K = 'k';
 	char A = 'a';
 	char T = 't';
@@ -120,14 +119,14 @@ void     root( void ) {
 	
 			sysclose( 1 );
 
-			for( int i = 0, i < 6, i++ ) {
+			for( int i = 0; i < 6; i++ ) {
 				if( input[i] != user[i] ) {
 					auth = FALSE;
 					break;
 				}
 			}
 
-			for( int i = 0, i < 16, i++ ) {
+			for( int i = 0; i < 16; i++ ) {
 				if( pass[i] != password[i] ) {
 					auth = FALSE;
 					break;
@@ -161,8 +160,144 @@ void     root( void ) {
 	
 
 
-		
-	
+/*
+    char  buff[100];
+    int pids[5];
+    int proc_pid, con_pid;
+    int i;
+
+    sysputs("Root has been called\n");
+
+
+    // Test for ready queue removal. 
+   
+    proc_pid = syscreate(&busy, 1024);
+    con_pid = syscreate(&busy, 1024);
+    sysyield();
+    //syskill(proc_pid);
+    sysyield();
+    //syskill(con_pid);
+
+    
+    for(i = 0; i < 5; i++) {
+      pids[i] = syscreate(&busy, 1024);
+    }
+
+    sysyield();
+    
+    //syskill(pids[3]);
+    sysyield();
+    //syskill(pids[2]);
+    //syskill(pids[4]);
+    sysyield();
+    //syskill(pids[0]);
+    sysyield();
+    //syskill(pids[1]);
+    sysyield();
+
+    syssleep(8000);;
+
+
+
+    kprintf("***********Sleeping no kills *****\n");
+    // Now test for sleeping processes
+    pids[0] = syscreate(&sleep1, 1024);
+    pids[1] = syscreate(&sleep2, 1024);
+    pids[2] = syscreate(&sleep3, 1024);
+
+    sysyield();
+    syssleep(8000);;
+
+
+
+    kprintf("***********Sleeping kill 2000 *****\n");
+    // Now test for removing middle sleeping processes
+    pids[0] = syscreate(&sleep1, 1024);
+    pids[1] = syscreate(&sleep2, 1024);
+    pids[2] = syscreate(&sleep3, 1024);
+
+    syssleep(110);
+    //syskill(pids[1]);
+    syssleep(8000);;
+
+    kprintf("***********Sleeping kill last 3000 *****\n");
+    // Now test for removing last sleeping processes
+    pids[0] = syscreate(&sleep1, 1024);
+    pids[1] = syscreate(&sleep2, 1024);
+    pids[2] = syscreate(&sleep3, 1024);
+
+    sysyield();
+    //syskill(pids[2]);
+    syssleep(8000);;
+
+    kprintf("***********Sleeping kill first process 1000*****\n");
+    // Now test for first sleeping processes
+    pids[0] = syscreate(&sleep1, 1024);
+    pids[1] = syscreate(&sleep2, 1024);
+    pids[2] = syscreate(&sleep3, 1024);
+
+    syssleep(100);
+    //syskill(pids[0]);
+    syssleep(8000);;
+
+    // Now test for 1 process
+
+
+    kprintf("***********One sleeping process, killed***\n");
+    pids[0] = syscreate(&sleep2, 1024);
+
+    sysyield();
+    //syskill(pids[0]);
+    syssleep(8000);;
+
+    kprintf("***********One sleeping process, not killed***\n");
+    pids[0] = syscreate(&sleep2, 1024);
+
+    sysyield();
+    syssleep(8000);;
+
+
+
+    kprintf("***********Three sleeping processes***\n");    // 
+    pids[0] = syscreate(&sleep1, 1024);
+    pids[1] = syscreate(&sleep2, 1024);
+    pids[2] = syscreate(&sleep3, 1024);
+
+
+    // Producer and consumer started too
+    proc_pid = syscreate( &producer, 4096 );
+    con_pid = syscreate( &consumer, 4096 );
+    sprintf(buff, "Proc pid = %d Con pid = %d\n", proc_pid, con_pid);
+    sysputs( buff );
+
+
+    processStatuses psTab;
+    int procs;
+    
+
+
+
+    syssleep(500);
+    procs = sysgetcputimes(&psTab);
+
+    for(int j = 0; j <= procs; j++) {
+      sprintf(buff, "%4d    %4d    %10d\n", psTab.pid[j], psTab.status[j], 
+	      psTab.cpuTime[j]);
+      kprintf(buff);
+    }
+
+
+    syssleep(10000);
+    procs = sysgetcputimes(&psTab);
+
+    for(int j = 0; j <= procs; j++) {
+      sprintf(buff, "%4d    %4d    %10d\n", psTab.pid[j], psTab.status[j], 
+	      psTab.cpuTime[j]);
+      kprintf(buff);
+    }
+
+    sprintf(buff, "Root finished\n");
+    sysputs( buff );*/
 
     sysstop();
     
